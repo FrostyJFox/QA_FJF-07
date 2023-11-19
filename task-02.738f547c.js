a50761fbd6b895500fb7b647913ff7f06de34c2d
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-19 15:41:50 +0000

    Deploying to gh-pages from @ FrostyJFox/QA_FJF-07@46497e89113033d347cb282319a2ab712b46d466 🚀

---
+++ task-02.738f547c.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("ingredients"),t=["Potatoes","Mushrooms","Garlic","Tomatoes","Herbs","Condiments"].map((e=>{const t=document.createElement("li");return t.textContent=e,t.classList.add("item"),t})),n=document.createDocumentFragment();n.append(...t),e.appendChild(n);
+//# sourceMappingURL=task-02.738f547c.js.map
